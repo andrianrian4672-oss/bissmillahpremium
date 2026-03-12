22a113832df11e22dc7a830b3a3ce1893ee7fca4
andrianrian4672-oss <andrianrian4672@gmail.com>
2026-03-12 14:03:39 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,33 +11,9 @@ module.exports = {
     { 
       name: "VIP-Lite", script: "botpremium.py", interpreter: "python3",
       env: { 
-        BOT_NAME: "PeaxelLite", 
+        BOT_NAME: "Peaxel_Lite", 
         API_KEY: process.env.API_LITE, 
         PRIVATE_KEY: process.env.PRIV_LITE 
-      }
-    },
-    { 
-      name: "VIP-Turbo", script: "botpremium.py", interpreter: "python3",
-      env: { 
-        BOT_NAME: "PeaxelTurbo", 
-        API_KEY: process.env.API_TURBO, 
-        PRIVATE_KEY: process.env.PRIV_TURBO 
-      }
-    },
-    { 
-      name: "VIP-Dex", script: "botpremium.py", interpreter: "python3",
-      env: { 
-        BOT_NAME: "PeaxelDex", 
-        API_KEY: process.env.API_DEX, 
-        PRIVATE_KEY: process.env.PRIV_DEX 
-      }
-    },
-    { 
-      name: "VIP-Solar", script: "botpremium.py", interpreter: "python3",
-      env: { 
-        BOT_NAME: "PeaxelSolar", 
-        API_KEY: process.env.API_SOLAR, 
-        PRIVATE_KEY: process.env.PRIV_SOLAR 
       }
     },
     { 
@@ -50,3 +26,4 @@ module.exports = {
     }
   ]
 };
+
